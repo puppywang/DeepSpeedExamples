6b61de5987428171a07899f8e551cc4897b84b5f
puppywang <puppywang@gmail.com>
2023-04-23 01:15:08 +0800

    prepare dataset.

--- applications/DeepSpeed-Chat/prepare_data.py
+++ applications/DeepSpeed-Chat/prepare_data.py
@@ -1,4 +1,6 @@
 from transformers import AutoModel, AutoTokenizer
+from datasets import load_dataset
+from datasets.download import DownloadConfig
 import transformers
 import sys
 
@@ -8,10 +10,25 @@ def download_model(model_path):
     AutoTokenizer.from_pretrained(model_path, resume_download=True)
     AutoModel.from_pretrained(model_path, resume_download=True)
 
+def download_dataset(dataset_path, name):
+    while True:
+        try:
+            load_dataset(dataset_path, name, download_config=DownloadConfig(resume_download=True), verification_mode='all_checks')
+            return
+        except ConnectionError as e:
+            print(f'Exception {e} when load dataset {dataset_path}')
+
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print(f'Usage:\n\t{sys.argv[0]} model_path')
+    if len(sys.argv) < 3:
+        print(f'Usage:\n\t{sys.argv[0]} m model_path|d dataset_path')
         exit(1)
-    print(f'Start to cache model_path = {sys.argv[1]}')
-    download_model(sys.argv[1])
-    print(f'Finish cache model_path = {sys.argv[1]}')
+
+    if sys.argv[1] == 'm':
+        print(f'Start to cache model_path = {sys.argv[2]}')
+        download_model(sys.argv[2])
+        print(f'Finish cache model_path = {sys.argv[2]}')
+    else:
+        print(f'Start to cache dataset_path = {sys.argv[2]}')
+        download_dataset(sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else None)
+        print(f'Finish cache dataset_path = {sys.argv[2]}')
